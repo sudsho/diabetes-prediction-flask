d0d9a7397b28a9e3e2d79767ae4f9d12f08ad063
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-07-06 12:21:57 +0530

    wip impute zeros as missing

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,4 +1,5 @@
 """Preprocessing for the Pima diabetes dataset."""
+import numpy as np
 import pandas as pd
 
 
@@ -17,4 +18,21 @@ TARGET_COL = "Outcome"
 
 def load_csv(path):
     df = pd.read_csv(path)
+    return df
+
+
+def replace_zeros_with_nan(df, cols):
+    """Several pima columns use 0 to mean missing. Convert those to NaN."""
+    df = df.copy()
+    for c in cols:
+        df[c] = df[c].replace(0, np.nan)
+    return df
+
+
+def median_impute(df, cols):
+    """Impute missing values in `cols` with the column median."""
+    df = df.copy()
+    for c in cols:
+        med = df[c].median()
+        df[c] = df[c].fillna(med)
     return df
