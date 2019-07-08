62883591a6d4622a12b2977441410bbe90620411
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-07-08 11:51:05 +0530

    implement train loop and metrics

--- src/train.py
+++ src/train.py
@@ -1,9 +1,15 @@
 """Train a single model based on the YAML config and save the joblib pickle."""
 import argparse
+import json
 import logging
+import os
 
+import joblib
 import yaml
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
 
+from src import model as model_lib
 from src import preprocess
 
 
@@ -14,7 +20,19 @@ log = logging.getLogger("train")
 def parse_args():
     p = argparse.ArgumentParser()
     p.add_argument("--config", default="configs/default.yaml")
+    p.add_argument("--model", default="logreg",
+                   help="logreg, random_forest, or xgboost")
     return p.parse_args()
+
+
+def evaluate(clf, X_test, y_test):
+    pred = clf.predict(X_test)
+    return {
+        "accuracy": accuracy_score(y_test, pred),
+        "precision": precision_score(y_test, pred),
+        "recall": recall_score(y_test, pred),
+        "f1": f1_score(y_test, pred),
+    }
 
 
 def main():
@@ -24,7 +42,33 @@ def main():
 
     log.info("loading %s", cfg["data"]["path"])
     df = preprocess.load_csv(cfg["data"]["path"])
-    log.info("rows=%d", len(df))
+    df = preprocess.replace_zeros_with_nan(df, cfg["data"]["zero_is_missing"])
+    df = preprocess.median_impute(df, cfg["data"]["zero_is_missing"])
+    X, y = preprocess.prepare_xy(df)
+    log.info("rows=%d features=%d", X.shape[0], X.shape[1])
+
+    sp = cfg["split"]
+    X_tr, X_te, y_tr, y_te = train_test_split(
+        X, y,
+        test_size=sp["test_size"],
+        random_state=sp["random_state"],
+        stratify=y if sp.get("stratify", True) else None,
+    )
+
+    params = cfg["models"][args.model]
+    clf = model_lib.build_model(args.model, params)
+    log.info("fitting %s", args.model)
+    clf.fit(X_tr, y_tr)
+
+    metrics = evaluate(clf, X_te, y_te)
+    log.info("metrics: %s", metrics)
+
+    out_dir = os.path.dirname(cfg["output"]["model_path"]) or "."
+    os.makedirs(out_dir, exist_ok=True)
+    joblib.dump(clf, cfg["output"]["model_path"])
+    with open(cfg["output"]["metrics_path"], "w") as fh:
+        json.dump(metrics, fh, indent=2)
+    log.info("saved %s", cfg["output"]["model_path"])
 
 
 if __name__ == "__main__":
