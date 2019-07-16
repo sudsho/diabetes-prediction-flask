0ec7a0ece94e1e12b1c8d6665138f9fe8bbd95e1
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-07-16 12:51:04 +0530

    scale features for logreg

--- src/model.py
+++ src/model.py
@@ -1,12 +1,21 @@
-"""Model factory. Returns an unfitted estimator given a model name and params."""
-from sklearn.linear_model import LogisticRegression
+"""Model factory. Returns an unfitted estimator given a model name and params.
+
+LogReg is wrapped in a Pipeline with StandardScaler since it is sensitive
+to feature scale. Tree-based models are returned bare.
+"""
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 
 
 def build_model(name, params):
     name = name.lower()
     if name == "logreg":
-        return LogisticRegression(**params)
+        return Pipeline([
+            ("scaler", StandardScaler()),
+            ("clf", LogisticRegression(**params)),
+        ])
     if name == "random_forest":
         return RandomForestClassifier(**params)
     if name == "xgboost":
