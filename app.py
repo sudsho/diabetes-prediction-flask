238f88bbddbd3ab0b053ece9352bfdecf6bd074f
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2019-07-29 23:00:50 +0530

    fix prod: handle missing model + health endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 """Flask app: simple HTML form -> diabetes prediction."""
+import logging
 import os
 
 from flask import Flask, render_template, request
@@ -9,6 +10,9 @@ from src.preprocess import FEATURE_COLS
 
 MODEL_PATH = os.environ.get("MODEL_PATH", "models/diabetes.pkl")
 
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("app")
+
 app = Flask(__name__)
 _clf = None
 
@@ -16,6 +20,11 @@ _clf = None
 def get_model():
     global _clf
     if _clf is None:
+        if not os.path.exists(MODEL_PATH):
+            raise RuntimeError(
+                "model file not found at %s. run compare_models first." % MODEL_PATH
+            )
+        log.info("loading model from %s", MODEL_PATH)
         _clf = load_model(MODEL_PATH)
     return _clf
 
@@ -27,14 +36,22 @@ def index():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    values = {c: request.form.get(c, 0) for c in FEATURE_COLS}
-    out = predict_one(get_model(), values)
+    try:
+        values = {c: request.form.get(c, 0) for c in FEATURE_COLS}
+        out = predict_one(get_model(), values)
+    except (ValueError, RuntimeError) as exc:
+        return render_template("error.html", message=str(exc)), 400
     return render_template(
         "result.html",
         values=values,
         prediction=out["prediction"],
         probability=out["probability"],
     )
+
+
+@app.route("/health")
+def health():
+    return {"status": "ok"}
 
 
 if __name__ == "__main__":
